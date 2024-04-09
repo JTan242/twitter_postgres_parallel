65b8c9f97c5e394eda59c31e9c03f3ac32ec02ac
JTan242 <jeremytan250@gmail.com>
2024-04-09 10:22:34 -0700

    update 2

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,16 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+#time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:2425 --inputs={} ::: $files
+
+
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time parallel python3 load_tweets_batch.py --db=postgresql://postgres:pass@localhost:2426/ --inputs={} ::: $files
